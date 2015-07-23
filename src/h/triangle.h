d52d3b49b9e14a89e017473ce02eca3cc8db0593
Sayutin Dmitry <cdkrot@yandex.ru>
2015-07-23 16:45:03 +0300

    added triangle.h

---
+++ src/h/triangle.h
@@ -0,0 +1,14 @@
+#ifndef TRIANGLE_INCL_H
+#define TRIANGLE_INCL_H
+
+// triangles must be clockwise.
+struct Triangle
+{
+	glm::vec3<float> a;
+	glm::vec3<float> b;
+	glm::vec3<float> c;
+	/* some texture info here */
+}
+
+
+#endif
