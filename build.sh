490095691ee3f1be8609d590173bd13e731a49f0
Sayutin Dmitry <cdkrot@yandex.ru>
2015-07-23 18:01:57 +0300

    Build.sh: bold red.

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ cd $(dirname $0)
 
 mkdir -p build
 cd build
-/bin/echo -e "\x1b[31m## running cmake  ##\x1b[0m"
+/bin/echo -e "\x1b[31;1m## running cmake  ##\x1b[0m"
 cmake ..
-/bin/echo -e "\x1b[31m## running make   ##\x1b[0m"
+/bin/echo -e "\x1b[31;1m## running make   ##\x1b[0m"
 make -j 3
-/bin/echo -e "\x1b[31m## running tests  ##\x1b[0m"
+/bin/echo -e "\x1b[31;1m## running tests  ##\x1b[0m"
 ./src/tests/run-tests
-/bin/echo -e "\x1b[31m## build succeded ##\x1b[0m"
+/bin/echo -e "\x1b[31;1m## build succeded ##\x1b[0m"
 
