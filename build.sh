5c2b850b118cfa4af83b1f11ba2cebdc3781979c
Sayutin Dmitry <cdkrot@yandex.ru>
2015-07-23 17:43:32 +0300

    Added build.sh script.

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+
+# simple build script. Should be run from project root.
+
+mkdir -p build
+cd build
+/bin/echo -e "\x1b[31m## running cmake  ##\x1b[0m"
+cmake ..
+/bin/echo -e "\x1b[31m## running make   ##\x1b[0m"
+make -j 3
+/bin/echo -e "\x1b[31m## running tests  ##\x1b[0m"
+./src/tests/run-tests
+/bin/echo -e "\x1b[31m## build succeded ##\x1b[0m"
+
